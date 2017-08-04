909f34dbad83d3ed6fa1a295ace434f607db3329
Sam Gordon <sbg2133@gmail.com>
2017-08-04 15:33:03 -0700

    Adding makefile for C script, loading in vectors from dat files

--- lic/lic.c
+++ lic/lic.c
@@ -1,5 +1,15 @@
+#include <assert.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include <errno.h>
 
+#define XMAX 100
+#define YMAX 100
+#define LINE_LEN 1024
+int xsize, ysize;
+float *X, *Y;
+float *dx, *dy;
 // pix_idx()
 // get_vector()
 // sl()
@@ -8,17 +18,80 @@
 // lic
 // image
 
-void pixIdx(vector_field_t *m_field, int* x_idx, int* y_idx)
+void coordGrid(float *X, float *Y)
 {
-    // Check that these are not pointing to NULL
-    assert(x_idx);
-    assert(y_idx);
-    for (int i = 0; i < XMAX 
-    *x_idx = 1;
-    *y_idx = 2;
+    // Make a linspace for X and Y
+    for (int i = 0; i < xsize; i++) {
+        X[i] = (float)(i)*XMAX / (xsize - 1);
+    }
+    for (int j = 0; j < ysize; j++) {
+        Y[j] = (float)(j)*YMAX / (ysize - 1);
+    }
+}
+
+void load_vectors(FILE* fd)
+{
+   char row[LINE_LEN];
+       while (fgets (row, LINE_LEN, fd)) puts(row);
+   return;
+}
+
+/** Given the input coordinates, return the grid index **/
+int pixIdx(float *m_coords, int *m_grid_idx)
+{
+    float tempx = (float)XMAX;
+    float tempy = (float)YMAX;
+    float diffx, diffy;
+    int i, j;
+    for (i = 0; i < xsize; i++) {
+	diffx = fabs(X[i] - m_coords[0]); 
+        // printf("i, Xi, xcoord, diffx = %d, %f, %f, %f, %f\n", i, X[i], m_coords[0], diffx, tempx);
+	if (diffx < tempx) {
+	    tempx = diffx;
+        } else { break; }
+    }
+    m_grid_idx[0] = i;
+    for (j = 0; j < ysize; j++) {
+        diffy = fabs(Y[j] - m_coords[1]); 
+        // printf("j, Yi, ycoord, diffy = %d, %f, %f, %f, %f\n", j, Y[j], m_coords[1], diffy, tempy);
+        if (diffy < tempy) {
+	    tempy = diffy;
+        } else { break; }
+    }
+    m_grid_idx[1] = j;
+    return 0;
 }
 
 int main (int argc, char *argv[])
 {
+    if(argc != 3) {
+        printf("Wrong number of arguments.\n");
+	exit(EXIT_FAILURE);
+    }
+    xsize = atoi(argv[1]);
+    ysize = atoi(argv[2]);
+    X = (float *)malloc(xsize*sizeof(float));
+    Y = (float *)malloc(ysize*sizeof(float));
+    dx = (float *)malloc(ysize*xsize*sizeof(float));
+    dy = (float *)malloc(ysize*xsize*sizeof(float));
+    coordGrid(X, Y);
+    
+    FILE* dx_fd = fopen("dx.dat", "r");
+    if (!dx_fd) {
+        perror("Error: "); 
+	exit(EXIT_FAILURE);
+    } else { load_vectors(dx_fd); }
+    fclose(dx_fd);
+    FILE* dy_fd = fopen("dy.dat", "r");
+    if (!dy_fd) {
+        perror("Error: "); 
+	exit(EXIT_FAILURE);
+    } else {load_vectors(dy_fd); }
+    fclose(dy_fd);
+
+    float coords[2] = {30.4, 20.3};
+    int grid_idx[2];
+    pixIdx(coords, grid_idx);
+    printf("xidx = %d, y_idx = %d\n", grid_idx[0], grid_idx[1]);
     return 0;
 }
